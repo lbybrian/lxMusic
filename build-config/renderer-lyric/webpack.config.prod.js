c2333a842754143974fcb91a3c179ece252451af
liubinyuan <liubinyuan@golaxy.cn>
2020-12-03 17:33:02 +0800

    copy-code已测试

---
+++ build-config/renderer-lyric/webpack.config.prod.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const webpack = require('webpack')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const { merge } = require('webpack-merge')
+
+const baseConfig = require('./webpack.config.base')
+
+const { dependencies } = require('../../package.json')
+
+let whiteListedModules = ['vue']
+
+
+module.exports = merge(baseConfig, {
+  mode: 'production',
+  devtool: false,
+  externals: [
+    ...Object.keys(dependencies || {}).filter(d => !whiteListedModules.includes(d)),
+  ],
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.join(__dirname, '../../src/static'),
+          to: path.join(__dirname, '../../dist/electron/static'),
+        },
+      ],
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"',
+      },
+    }),
+  ],
+  optimization: {
+    chunkIds: 'named',
+    minimizer: [
+      new TerserPlugin(),
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+  },
+  performance: {
+    hints: 'warning',
+  },
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+})
+
+
---
+++ src/main/events/_name.js
@@ -0,0 +1,35 @@
+exports.common = {
+  initConfig: 'initConfig',
+  config: 'config',
+}
+
+exports.mainWindow = {
+  name: 'mainWindow',
+  setLyricInfo: 'setLyricInfo',
+  destroy: 'destroy',
+  quit: 'quit',
+  toggle_minimize: 'toggle_minimize',
+  toggle_hide: 'toggle_hide',
+  ready_to_show: 'ready_to_show',
+  show: 'show',
+  hide: 'hide',
+}
+
+exports.tray = {
+  name: 'tray',
+  create: 'create',
+  destroy: 'destroy',
+}
+
+exports.winLyric = {
+  name: 'winLyric',
+  create: 'create',
+  close: 'close',
+}
+
+exports.hotKey = {
+  name: 'hotKey',
+  init: 'init',
+  config: 'config',
+  keyDown: 'keyDown',
+}
